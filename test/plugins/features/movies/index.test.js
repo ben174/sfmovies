820ac8dabae4091cb646c6de4970d7c96f4caad5
Ben Friedland <ben174@gmail.com>
2019-03-05 15:04:05 -0800

    feat(movies): creates a Movie controller (#6)

---
+++ test/plugins/features/movies/index.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Movies = require('../../../../lib/server');
+
+describe('movies integration', () => {
+
+  describe('create', () => {
+
+    it('creates a movie', () => {
+      return Movies.inject({
+        url: '/movies',
+        method: 'POST',
+        payload: { title: 'Volver' }
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+        expect(response.result.object).to.eql('movie');
+      });
+    });
+
+  });
+
+});

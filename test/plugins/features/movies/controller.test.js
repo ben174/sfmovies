d4e78e3b553d6afd4fce82b8bf19b86c300fdd15
Ben Friedland <ben174@gmail.com>
2019-03-06 08:43:40 -0800

    Validator (#12)
    * feat(movies): adds validator to movies endpoint
    
    * feat(movies): adds validator tests for /movie (#5)
    
    * feat(movies): creates a Movie controller (#6)

---
+++ test/plugins/features/movies/controller.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Controller = require('../../../../lib/plugins/features/movies/controller');
+
+describe('movie controller', () => {
+
+  describe('create', () => {
+
+    it('creates a movie', async () => {
+      const payload = { title: 'WALL-E' };
+
+      const movie = await Controller.create(payload);
+
+      expect(movie.get('title')).to.eql(payload.title);
+    });
+
+  });
+
+});
